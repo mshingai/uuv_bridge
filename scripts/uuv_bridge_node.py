b85be440d4c1b4c54447e17b828bf0156725091a
shingai <shingai.masatoshi@gmail.com>
2020-09-09 10:08:22 +0900

    変換リストを使った実装に変更

--- scripts/uuv_bridge_node.py
+++ scripts/uuv_bridge_node.py
@@ -3,20 +3,32 @@ import rospy
 from std_msgs.msg import Float64
 from uuv_gazebo_ros_plugins_msgs.msg import FloatStamped
 
-def callback(data):
-    rospy.loginfo(rospy.get_caller_id() + " Iheard %f", data.data)
-    pub = rospy.Publisher('lauv/fins/0/input', FloatStamped, queue_size=10)
-    header = rospy.Time.now()
-    pubdata = FloatStamped()
-    pubdata.header.stamp = header
-    pubdata.data = data.data
-    rospy.loginfo("header = %s, data = %f", header, pubdata.data)
-    pub.publish(pubdata)
+def actuator_callback(inTopic, publisher):
+    pub = publisher
 
-def listener():
+    def callback(data):
+        rospy.loginfo(rospy.get_caller_id() + " I heard %f", data.data)
+
+        pubdata = FloatStamped()
+        pubdata.header.stamp = rospy.Time.now()
+        pubdata.data = data.data
+        
+        pub.publish(pubdata)
+    return callback
+
+def converter(list):
     rospy.init_node('rosbridge_node', anonymous=True)
-    rospy.Subscriber("lauv/fins/fin0/input", Float64, callback)
+
+
+    for item in list:
+        callback = actuator_callback(item[0], rospy.Publisher(item[1], FloatStamped, queue_size=10))
+        rospy.Subscriber(item[0], Float64, callback)
+
     rospy.spin()
 
 if __name__ == '__main__':
-    listener()
+    convert_list = [
+        ('/lauv/fins/fin0/input', '/lauv/fins/0/input'),
+        ('/lauv/fins/fin1/input', '/lauv/fins/0/input'),
+    ]
+    converter(convert_list)
