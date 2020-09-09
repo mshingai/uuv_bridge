ab732ec4f592130862142f824c0d5864dbf68b45
shingai <shingai.masatoshi@gmail.com>
2020-09-09 10:41:56 +0900

    コメント追加

--- scripts/uuv_bridge_node.py
+++ scripts/uuv_bridge_node.py
@@ -3,11 +3,12 @@ import rospy
 from std_msgs.msg import Float64
 from uuv_gazebo_ros_plugins_msgs.msg import FloatStamped
 
+# Subscriber用のコールバックを生成するクロージャー
 def actuator_callback(inTopic, publisher):
     pub = publisher
 
     def callback(data):
-        rospy.loginfo(rospy.get_caller_id() + " I heard %f", data.data)
+        # rospy.loginfo(rospy.get_caller_id() + " I heard %f", data.data)
 
         pubdata = FloatStamped()
         pubdata.header.stamp = rospy.Time.now()
@@ -19,7 +20,7 @@ def actuator_callback(inTopic, publisher):
 def converter(list):
     rospy.init_node('rosbridge_node', anonymous=True)
 
-
+    # 変換リストからPublisher，SubScriberを設定
     for item in list:
         callback = actuator_callback(item[0], rospy.Publisher(item[1], FloatStamped, queue_size=10))
         rospy.Subscriber(item[0], Float64, callback)
@@ -28,7 +29,12 @@ def converter(list):
 
 if __name__ == '__main__':
     convert_list = [
+        # 変換元をSubscribeし，データをそのまま変換先へPublishする
+        # 変換元，　　　　　　　　　　　　変換先
         ('/lauv/fins/fin0/input', '/lauv/fins/0/input'),
-        ('/lauv/fins/fin1/input', '/lauv/fins/0/input'),
+        ('/lauv/fins/fin1/input', '/lauv/fins/1/input'),
+        ('/lauv/fins/fin2/input', '/lauv/fins/2/input'),
+        ('/lauv/fins/fin3/input', '/lauv/fins/3/input'),
+        ('/lauv/thrusters/thruster0/input', '/lauv/thrusters/0/input'),
     ]
     converter(convert_list)
